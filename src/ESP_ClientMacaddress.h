fd2ff432f423cb69ae9e4b56a6d615b79b37e1c3
Roger Moschiel <45469807+moschiel@users.noreply.github.com>
2019-03-11 18:38:32 -0300

    Update ESP_ClientMacaddress.h

--- src/ESP_ClientMacaddress.h
+++ src/ESP_ClientMacaddress.h
@@ -1,23 +1,39 @@
 #ifndef ESP_ClientMacaddress_h
 #define ESP_ClientMacaddress_h
 
+#ifndef EEPROM_ADDR_MAC_LIST
+#define EEPROM_ADDR_MAC_LIST 0
+#endif
+
+#ifndef MAX_LIST
+#define MAX_LIST 10
+#endif
+
+#ifndef MAX_MACADDRESS
+#define MAX_MACADDRESS 30
+#endif
+
 #include "Arduino.h"
 #include <stdint.h>
 #include <WiFi.h>
 #include "esp_wifi.h"
 
-#define MAX_MACADDRESS 50
-
 class ClientMacaddress{
     public:
         ClientMacaddress();
-        ClientMacaddress(uint8_t (*_mac_list_)[6], size_t rows);
+        ClientMacaddress(uint8_t (*predetermined_mac_list)[6], size_t rows);
+        void putAddr(int list_index, uint8_t *mac);
         uint8_t *getAddr(WiFiClient client); 
         bool isKnown(uint8_t *mac); 
         bool compare(uint8_t *mac1, uint8_t *mac2);
 
     private:
-        int _macaddressList_[MAX_MACADDRESS][6];
-        int Num_Macaddress = 0;    
+        int predetermined_list[MAX_MACADDRESS][6];
+        int predetermined_list_size = 0;
+        struct list{
+            int size;
+            int macaddress[MAX_MACADDRESS][6];
+        }; 
+        struct list lists[MAX_LIST];    
 };
 #endif
