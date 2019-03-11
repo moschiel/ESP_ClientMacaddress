fea6cbd74604d15a33c46219e11d24d313ce78f4
Roger Moschiel <45469807+moschiel@users.noreply.github.com>
2019-03-11 18:38:00 -0300

    Update ESP_ClientMacaddress.cpp

--- src/ESP_ClientMacaddress.cpp
+++ src/ESP_ClientMacaddress.cpp
@@ -3,18 +3,18 @@
 ClientMacaddress::ClientMacaddress(){
 }
 
-ClientMacaddress::ClientMacaddress(uint8_t (*_mac_list_)[6], size_t rows){
-    this->Num_Macaddress = rows;
+ClientMacaddress::ClientMacaddress(uint8_t (*predetermined_mac_list)[6], size_t rows){
+    this->predetermined_list_size = rows;
 
     for (size_t i=0; i<rows; ++i){
         for (size_t j = 0; j < 6; ++j){
-            this->_macaddressList_[i][j] = _mac_list_[i][j];
+            this->predetermined_list[i][j] = predetermined_mac_list[i][j];
         }
     }
 }
 
-uint8_t *ClientMacaddress::getAddr(WiFiClient _client_){
-    IPAddress _client_ip_ = _client_.remoteIP();
+uint8_t *ClientMacaddress::getAddr(WiFiClient client){
+    IPAddress client_ip = client.remoteIP();
     wifi_sta_list_t stationList;
     tcpip_adapter_sta_list_t adapter_sta_list;
 
@@ -24,32 +24,31 @@ uint8_t *ClientMacaddress::getAddr(WiFiClient _client_){
     esp_wifi_ap_get_sta_list(&stationList);
     tcpip_adapter_get_sta_list(&stationList, &adapter_sta_list);  
 
-    IPAddress _ip_from_list_;
-    static uint8_t _mac_addr_[6];
-    memset(_mac_addr_, 0, 6);
+    IPAddress ip_from_list;
+    static uint8_t mac_addr[6];
+    memset(mac_addr, 0, 6);
 
     for(int i = 0; i < stationList.num; i++){
         wifi_sta_info_t station = stationList.sta[i];
         tcpip_adapter_sta_info_t adapter = adapter_sta_list.sta[i];
 
-        _ip_from_list_.fromString(ip4addr_ntoa(&(adapter.ip)));
+        ip_from_list.fromString(ip4addr_ntoa(&(adapter.ip)));
         
-        if (_client_ip_ == _ip_from_list_){
-            memcpy(_mac_addr_, station.mac, 6);
-            return _mac_addr_;
+        if (client_ip == ip_from_list){
+            memcpy(mac_addr, station.mac, 6);
+            return mac_addr;
         }     
     }
 
-    return _mac_addr_;
-
+    return mac_addr;
 }
 
 bool ClientMacaddress::isKnown(uint8_t *mac){
     uint8_t temp_mac[6];
 
-    for (int i=0; i<this->Num_Macaddress;i++){
+    for (int i=0; i<this->predetermined_list_size;i++){
         for(int k = 0; k < 6; k++)
-            temp_mac[k] = this->_macaddressList_[i][k]; 
+            temp_mac[k] = this->predetermined_list[i][k]; 
 
         if(this->compare(mac, temp_mac))
             return true;
